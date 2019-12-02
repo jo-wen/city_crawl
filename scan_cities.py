0bfe9d92578138d428ae9e4e5164e7fd5e2f0ee5
josh <josh@backblaze.com>
2019-12-02 11:58:53 -0800

    added some nmappable url lists and scan results

--- scan_cities.py
+++ scan_cities.py
@@ -58,18 +58,18 @@ with open(csv_file) as csvfile:
 
 f_cities = open(f_city, 'a')
 for city in all_cities:
-    check_gov = "http://{}.gov".format(city.lower().replace(" ", ""))
-    check_us = "http://{}.us".format(city.lower().replace(" ", ""))
-    check_com = "http://{}.com".format(city.lower().replace(" ", ""))
-    check_gov_https = "https://{}.gov".format(city.lower().replace(" ", ""))
-    check_us_https = "https://{}.us".format(city.lower().replace(" ", ""))
-    check_com_https = "https://{}.com".format(city.lower().replace(" ", ""))
+    check_gov = "{}.gov".format(city.lower().replace(" ", ""))
+    check_us = "{}.us".format(city.lower().replace(" ", ""))
+    check_com = "{}.com".format(city.lower().replace(" ", ""))
+    # check_gov_https = "https://{}.gov".format(city.lower().replace(" ", ""))
+    # check_us_https = "https://{}.us".format(city.lower().replace(" ", ""))
+    # check_com_https = "https://{}.com".format(city.lower().replace(" ", ""))
     f_cities.write(check_gov + '\n')
     f_cities.write(check_us + '\n')
     f_cities.write(check_com + '\n')
-    f_cities.write(check_gov_https + '\n')
-    f_cities.write(check_us_https + '\n')
-    f_cities.write(check_com_https + '\n')
+    # f_cities.write(check_gov_https + '\n')
+    # f_cities.write(check_us_https + '\n')
+    # f_cities.write(check_com_https + '\n')
 
 # with www
 for city in all_cities:
@@ -88,7 +88,11 @@ for city in all_cities:
 f_cities.close()
 
 
-
+f_checked = open('checked_cities', 'a')
+for line in open(f_city, 'r', newline="\n"):
+    print(line)
+    print(req.urlopen(line).reason())
+    break
 gov = urllib.request.urlopen(check_gov)
 us = urllib.request.urlopen(check_us)
 com = urllib.request.urlopen(check_com)
